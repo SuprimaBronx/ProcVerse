3d9cbbdbdd21b341bc6fa665c81fa9ed88380c75
SuprimaBronx <k7iao@yandex.ru>
2021-04-26 02:01:06 +0700

    Added filter

--- containers.py
+++ containers.py
@@ -21,6 +21,15 @@ def out(file , container):
         file.write('мощность двигателя: ' + str(container[i].engine_power) + "\n")
     print('всё')
 
+
+def out_filtr(file, cars):
+    file.write('Контейнер содержит ' + str(len(cars)) + ' элементов \n')
+    for i in range(len(cars)):
+        if cars[i].type == 'truck':
+            out_cars(cars[i], file, i)
+            file.write('мощность двигателя: ' + str(cars[i].engine_power) + "\n")
+
+
 def clear(cars):
     cars = []
     return cars

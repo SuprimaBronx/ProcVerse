46faf772cca1b18e2d27ea4b43d7a1feed6aadab
SuprimaBronx <k7iao@yandex.ru>
2021-05-29 21:10:36 +0700

    multimethods first

--- containers.py
+++ containers.py
@@ -14,12 +14,24 @@ def in_data(file_input, cars):
         in_cars(temp, cars)
 
 
-def out(file , container):
+def out(file, container):
     file.write('Контейнер содержит ' + str(len(container)) + ' элементов \n')
     for i in range(len(container)):
         out_cars(container[i], file, i)
         file.write('мощность двигателя: ' + str(container[i].engine_power) + "\n")
     print('всё')
+
+
+def multimethods(cars, out_name):
+    objects = {
+        Truck: 'Truck',
+        Bus: 'Bus'
+    }
+    for i in range(0, len(cars), 2):
+        res = ''
+        res += objects[type(cars[i])] + ' and ' + objects[type(cars[i + 1])]
+        out_name.write(res + '\n')
+
 
 def clear(cars):
     cars = []
--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from containers import *
 def main():
     input_name = str(input())
     output_name = str(input())
+    mul_out = str(input())
     file = open(input_name)
     container = Container()
     in_data(file, container.cars)
+    file.close()
+    file = open(mul_out,'w')
+    multimethods(container.cars,file)
     file.close()
     file = open(output_name, 'w')
     out(file, container.cars)
